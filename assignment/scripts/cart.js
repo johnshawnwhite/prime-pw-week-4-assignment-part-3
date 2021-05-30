d47f374ecf22cc3b851cf2177920188574a82e12
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-05-30 04:29:59 -0400

    first thing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,14 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+console.log(basket);
+// checked to make sure array is complete
+function additem() {
+  var string= 'item';
+  basket.push('basket');
+  return (true);
+
+}
+console.log('testing first option',additem());
+//first attempt base model works fine..
