40d3ff83436cb4ee0de4b2381af58a99264f96fe
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-05-30 10:38:41 -0400

    issues

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,9 +6,29 @@ console.log(basket);
 // checked to make sure array is complete
 function additem() {
   var string= 'item';
-  basket.push('basket');
+  basket.push('oranges');
   return (true);
 
 }
-console.log('testing first option',additem());
+console.log('Item was added',additem());
 //first attempt base model works fine..
+console.log(`Basket is ${basket}`);
+//testing with example
+
+function listItems() {
+} for (let i=0; i<=5; i++) {
+  console.log listItems([i]);
+}
+
+
+function empty(){
+  return true;
+}
+
+
+console.log('add items to basket');
+for (let i=0; i<6; i++) {
+  console.log(i);
+  return ('Adding apples (expect true)',additem('apples'));
+}
+//Line 20 is incorrect** got to fix*
