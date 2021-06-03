8cfb336e7742781105578ae6b9269cf5a5bb2dea
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-06-03 17:58:02 -0400

    mando done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,29 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+let basket = [];  /// global variable//
+const MAX_ITEMS = 3;
 console.log(basket);
 // checked to make sure array is complete
-function additem(oranges) {
-  var string= 'item';
-  basket.push('oranges');
+function additem(item) {
+  basket.push(item); //// nothing added yet, have not called function
   return (true);
-
 }
-console.log('Item was added',additem());
+
+//
+
+additem('oranges');
 //first attempt base model works fine..
 console.log(`Basket is ${basket}`);
 //testing with example
-
-basket.push('apples');
-basket.push('pears');
+additem('bananas');
+console.log(`basket is ${basket}`);
+additem('blueberries');
+console.log(`basket is ${basket}`);
+additem('pears');
 function listItems() {
 } for (let i=0; i<basket.length; i++) {
-  console.log (basket[i]);
+  console.log (basket);
 }
-// Added another item so it wasnt just 2, i think i am having to look
-// through old assignments to often for completion. going to re work first weeks stuff
-// starting tommorrow.
 function empty (){
   basket.length = 0;
 }
